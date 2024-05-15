3070145ed372e6fc6c3c97744275f51b02f267c8
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-05-15 13:21:11 +0500

    skills modified

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development","React Js.", "web development", "Video Editing","Photo Editing"],
+    strings: ["frontend development","React Js.", "web development", "Video Editing","Photo Editing", "Data Analysis"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
