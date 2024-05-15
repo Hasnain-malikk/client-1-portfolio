9b5060c74d732c7ce86d7f0aa1cc0cfa8269c615
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-05-15 21:43:49 +0500

    fav left

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Malik Hasnain";
+            document.title = "Portfolio | Abigail Salimpuran";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
